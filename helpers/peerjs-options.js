b795d8e1b97a5bab24361f603477cda0da1feceb
wavwarehousedev <admin@wavwarehouse.com>
2025-03-03 11:45:35 -0600

    using free peerjs server

--- helpers/peerjs-options.js
+++ helpers/peerjs-options.js
@@ -1,7 +1,9 @@
-const peerJsOptions = isLocal() ? { debug: 1 } : {
+const peerJsOptions = isLocal() ? { debug: 1 } : {debug: 0}
+
+/*{
     host: 'dndcombatapp-413415.uc.r.appspot.com',
     port: 443,
     path: '/',
     secure: true,
     debug: 0
-}
+}*/
